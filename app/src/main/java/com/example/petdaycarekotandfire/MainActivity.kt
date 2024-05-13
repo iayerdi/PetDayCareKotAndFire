02010e7c15f3a6f173d4d00a9b12c914b7db7048
inigo <inigoayerdi@gmail.com>
2024-05-13 19:51:26 +0200

    main activity

--- app/src/main/java/com/example/petdaycarekotandfire/MainActivity.kt
+++ app/src/main/java/com/example/petdaycarekotandfire/MainActivity.kt
@@ -1,18 +1,26 @@
 package com.example.petdaycarekotandfire
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.result.contract.ActivityResultContracts
 import com.google.firebase.analytics.FirebaseAnalytics
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var i = Intent(this,Login::class.java)
-        startActivity(i)
+        val buttonIniciar = findViewById<Button>(R.id.buttonIniciar)
+
+        buttonIniciar.setOnClickListener {
+            var i = Intent(this,Login::class.java)
+            startActivity(i)
+        }
+
     }
 
 }
